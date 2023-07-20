90f2cdff57a15c6ab230111bb10812a5fe910f1a
Minna <liu_minna@hotmail.com>
2023-07-20 16:34:47 +0200

    updates more solutions

--- Contains Duplicate/Contains Duplicate.js
+++ Contains Duplicate/Contains Duplicate.js
@@ -12,3 +12,19 @@ var containsDuplicate = function(nums) {
     }
     return false;
   };
+
+  var containsDuplicate = function(nums) {
+    const newSet = new Set(nums);
+    const isUnique = newSet.size === nums.length;
+    return !isUnique;
+  };
+
+  var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let a = 0; a < i; a++) {
+        const isDuplicate = nums[a] === nums[i];
+        if (isDuplicate) return true;
+      }
+    }
+        return false;
+  };
