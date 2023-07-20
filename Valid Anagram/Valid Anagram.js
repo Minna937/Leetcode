7bc1507ff27a823a28ad05dec54386fa90752f1b
Minna <liu_minna@hotmail.com>
2023-07-20 17:20:13 +0200

    adds soluton to anagram

--- Valid Anagram/Valid Anagram.js
+++ Valid Anagram/Valid Anagram.js
@@ -3,26 +3,41 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const charFrequency = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    charFrequency[char] = charFrequency[char] + 1 || 1;
+  }
+
+
+  for (let i = 0; i < t.length; i++) {
+    const char = t[i];
+    if (!charFrequency[char]) {
       return false;
     }
-  
-    const charFrequency = {};
-  
-    for (let i = 0; i < s.length; i++) {
-      const char = s[i];
-      charFrequency[char] = charFrequency[char] + 1 || 1;
-    }
-  
-  
-    for (let i = 0; i < t.length; i++) {
-      const char = t[i];
-      if (!charFrequency[char]) {
-        return false;
-      }
-      charFrequency[char]--;
-    }
-  
-    return true;
-  };
+    charFrequency[char]--;
+  }
+
+  return true;
+};
+
+if (s.length !== t.length) {
+  return false;
+};
+
+var isAnagram = function (s, t) {
+  const reorder = (str) => {
+    return str
+      .split("")
+      .sort((a, b) => a.localeCompare(b))
+      .join("");
+  }
+
+  return reorder(s) === reorder(t);
+};
